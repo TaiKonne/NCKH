21e471d076ed5f2b7799cda482c1e88ea14e27d6
TaiKonne <2124802010067@student.tdmu.edu.vn>
2022-12-14 11:56:00 +0700

    lỗi lum la đang tìm cách fix

--- Project/index.js
+++ Project/index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+// import { name as appName } from './app.json';
+import ProfileScreen from './profile';
+AppRegistry.registerComponent(appName, () => ProfileScreen);
---
+++ Project/profile.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Test, StyleSheet } from 'react-native';
+import md5 from 'blueimp-md5';
+import { Avatar, Grid, Row, Col } from 'react-native-elements';
+
+
+export default class ProfileScreen extends Component {
+    gravatarUrl() {
+        let email = 'markus@mmuehlberger.com';
+        return 'https://gravatar.com/avatar/' + md5(email) + '?s=400';
+    }
+    render() {
+        return (
+            <Grid>
+                <Row style={{alignItems:'center'}}>
+                    <Avatar
+                        large
+                        rounded
+                        source={{ uri: this.gravatarUrl() }}
+                        containerStyle={{width:75,height:75,marginVertical:10}}
+                    />
+                </Row>
+                <Row>
+                    <Col>
+                        <Text> 123 following me</Text>
+                    </Col>
+                    <Col>
+                        <Text> 456 following me</Text>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
+}
