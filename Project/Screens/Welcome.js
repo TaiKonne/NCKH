b545fe1d09b2ba0267403abf825737b69b46a53b
TaiKonne <2124802010067@student.tdmu.edu.vn>
2022-12-15 20:27:01 +0700

    fix được lỗi và đang tìm hiểu về mainscreen

--- Project/Screens/Welcome.js
+++ Project/Screens/Welcome.js
@@ -9,63 +9,95 @@ function Welcome(props) {
         flex: 100
     }}>
         <ImageBackground source={
-            require('../front_end/background.jpg')
+            require('../front_end/ez_background.jpg')
         }
             resizeMode='cover'
             style={{
                 flex: 100,
-
             }}
         >
             <View style={{
-                flexDirection: 'row',
-                flex: 8,
-                backgroundColor: 'green',
-                justifyContent: 'flex-start',
-                alignItems: 'center'
-            }}>
-                <Image
-                    source={require('../front_end/icon_fire.png')}
-                    style={{
-                        width: 60,
-                        height: 70,
-                        // alignSelf:'center'
-                        // marginHorizontal:10 cho xung quanh khoản cách là 10px
-                    }}
-                />
 
-                <Text style={{
-                    color: 'white'
+                flex: 8,
+
+
+            }}>
+                <View style={{
+                    flexDirection: 'row',
+                    height: 70,
+                    justifyContent: 'flex-start',
+                    alignItems: 'center'
                 }}>
-                    Welcome to the world
-                </Text>
-                <View style={{ flex: 1 }} />
-                <Image
-                    source={require('../front_end/icon_question.png')}
-                    style={{
-                        width: 30,
-                        height: 30,
-                        // tintColor:'white'
-                        marginStart: 90
-                    }}
-                />
+                    <Image
+                        source={require('../front_end/icon_fire.png')}
+                        style={{
+                            width: 60,
+                            height: 70,
+                            // alignSelf:'center'
+                            // marginHorizontal:10 cho xung quanh khoản cách là 10px
+                        }}
+
+                    />
+                    <Text style={{
+                        color: 'white'
+                    }}>
+                        Welcome to the world
+                    </Text>
+                    <View style={{ flex: 1 }} />
+                    <Image
+                        source={require('../front_end/icon_question.png')}
+                        style={{
+                            width: 30,
+                            height: 30,
+                            // tintColor:'white'
+                            marginStart: 90
+                        }}
+                    />
+                </View>
+
             </View>
             <View style={{
-                flex: 20,
+                flex: 15,
                 width: '100%',
-                backgroundColor: 'yellow'
-            }} />
+                backgroundColor: 'skyblue',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Text
+                    style={{
+                        fontWeight: 'bold',
+                        fontSize: 13
+                    }}>
+                    Welcome to main screen!!
+                </Text>
+                <Text
+                    style={{
+                        marginBottom: 7,
+                        color: 'white',
+                        fontWeight: 'bold',
+                        fontSize: 16
+                    }}>SOCIAL NETWORK OF THU DAU MOT UNIVERSITY
+                </Text>
+                <Text
+                    style={{
+                        marginBottom: 7,
+                        color: 'white',
+                        fontWeight: 'bold',
+                        fontSize: 14
+                    }}>Please select your account type!
+                </Text>
+            </View>
             <View style={{
-                backgroundColor: 'blue',
-                flex: 20
+                backgroundColor: 'yellow',
+                flex: 30
             }} />
             <View style={{
                 backgroundColor: 'white',
                 flex: 20
             }} />
-        </ImageBackground>
+        </ImageBackground >
 
-    </View>
+    </View >
 }
 export default Welcome
 
