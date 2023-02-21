e247aae32dead1a7cc2e01fada98e224aae89c53
TaiKonne <taichuotchuoi@gmail.com>
2023-02-21 16:17:05 +0700

    21/02/23 i detached constants screen home status

---
+++ Project/Screens/Constants_Home.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import { SearchBar } from 'react-native-elements';
+import {
+    Image,
+    Text,
+    View,
+    ImageBackground,
+    TouchableOpacity,
+    ScrollView, StyleSheet
+} from 'react-native'
+
+import Icon from 'react-native-vector-icons/dist/FontAwesome'
+import { Avatar } from 'react-native-paper';
+import Video from 'react-native-video'
+import VideoPlayer from 'react-native-video-controls'
+/*
+constant: text and !image and !video
+constant: text and image and !video
+constant: text and !image and video
+*/
+function Constants_Home(props) {
+    let { video, ima } = props.cons
+    if (video == '' && ima == '') {
+    }
+    else if (video == '') {
+        return (
+            <View style={{ // video or image
+                alignItems: 'center',
+
+            }}>
+                <Image source={{ uri: ima }} style={{
+                    width: '100%',
+                    height: 400
+                }} />
+            </View>
+        );
+    }
+    else if (ima == '') {
+        return (
+            <View style={styles.container}>
+                <Video source={{ uri: video }}
+                    style={styles.backgroundVideo}
+                    paused={true}
+                    controls={true}
+                />
+            </View>
+        )
+    }
+};
+
+var styles = StyleSheet.create({ // style video anh Minh cầu nguyện
+    container: {
+        backgroundColor: 'black',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 'auto',
+        height: 400,
+    },
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+
+    },
+});
+
+
+/*
+------------------------------------------------- status
+                    <View style={{ // text
+                        marginVertical: 5,
+                        // borderWidth: 1,
+                        // borderColor: 'yellow',
+                        color: 'black',
+                        marginLeft: 6,
+                        marginRight: 6,
+                    }}>
+                        <Text
+                            style={{ //hashtag
+                                fontSize: 16,
+                                color: 'black',
+                            }}>
+                            {status}
+                        </Text>
+                    </View>
+-------------------------------------------------------- ima
+                    <View style={{ // video or image
+                        alignItems: 'center',
+
+                    }}>
+                        <Image source={{ uri: ima }} style={{
+                            width: '100%',
+                            height: 400
+                        }} />
+                    </View>
+-------------------------------------------------------- video
+                <View style={styles.container}>
+                    <Video source={{ uri: video }}
+                        style={styles.backgroundVideo}
+                        controls={true}
+                    />
+                </View>
+
+*/
+export default Constants_Home;
--- Project/Screens/Homeid.js
+++ Project/Screens/Homeid.js
@@ -13,158 +13,21 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome'
 import { Avatar } from 'react-native-paper';
 import Video from 'react-native-video'
 import VideoPlayer from 'react-native-video-controls'
-/*   done
-constant: text and !image and !video
-constant: text and image and !video
-constant: text and !image and video
-*/
+import Constants_Home from './Constants_Home.js'
+
+
 function Homeid(props) {
     let { name, avatar, status, video, ima, interact } = props.user //destructuring
-    if (video == '' && ima == '') {
-        return (
-            <View>
-                <View style={{  // status
-                    // borderWidth: 1,
-                    // borderColor: 'black',
-                    height: 'auto',
-                    marginVertical: 6,
-                    justifyContent: 'center',
-                    backgroundColor: 'white'
-                    // alignItems: 'center',
-                }}>
-                    <View style={{ // avatar and content status
-                        height: 50,
-                        // borderWidth: 1,
-                        // borderColor: 'green',
-                        width: '100%',
-                        flexDirection: 'row',
-                        marginLeft: 6,
-                        marginRight: 6,
-                    }}>
-                        <Avatar.Image size={50} source={{
-                            uri: avatar
-                        }} />
-                        <Text style={{
-                            fontWeight: 'bold',
-                            color: 'black',
-                            fontSize: 15,
-                            height: 40,
-                            marginLeft: 12,
-                            marginTop: 5
-                        }}>
-                            {name}
-                        </Text>
-                    </View>
-                    <View style={{ // text
-                        marginVertical: 5,
-                        // borderWidth: 1,
-                        // borderColor: 'yellow',
-                        color: 'black',
-                        marginLeft: 6,
-                        marginRight: 6,
-                    }}>
-                        <Text
-                            style={{ //hashtag
-                                fontSize: 16,
-                                color: 'black',
-                            }}>
-                            {status}
-                        </Text>
-                    </View>
-
-                    <View style={{
-                        borderWidth: 1,
-                        borderColor: 'green',
-                        height: 70
-                    }}>
-
-                    </View>
-                </View>
-            </View>
-        )
-    }
-    else if (video == '') {
-        return (
-            <View>
-                <View style={{  // status
-                    // borderWidth: 1,
-                    // borderColor: 'black',
-                    height: 'auto',
-                    marginVertical: 6,
-                    justifyContent: 'center',
-                    backgroundColor: 'white'
-                    // alignItems: 'center',
-                }}>
-                    <View style={{ // avatar and content status
-                        height: 50,
-                        // borderWidth: 1,
-                        // borderColor: 'green',
-                        width: '100%',
-                        flexDirection: 'row',
-                        marginLeft: 6,
-                        marginRight: 6,
-                    }}>
-                        <Avatar.Image size={50} source={{
-                            uri: avatar
-                        }} />
-                        <Text style={{
-                            fontWeight: 'bold',
-                            color: 'black',
-                            fontSize: 15,
-                            height: 40,
-                            marginLeft: 12,
-                            marginTop: 5
-                        }}>
-                            {name}
-                        </Text>
-                    </View>
-                    <View style={{ // text
-                        marginVertical: 5,
-                        // borderWidth: 1,
-                        // borderColor: 'yellow',
-                        color: 'black',
-                        marginLeft: 6,
-                        marginRight: 6,
-                    }}>
-                        <Text
-                            style={{ //hashtag
-                                fontSize: 16,
-                                color: 'black',
-                            }}>
-                            {status}
-                        </Text>
-                    </View>
-
-                    <View style={{ // video or image
-                        alignItems: 'center',
-
-                    }}>
-                        <Image source={{ uri: ima }} style={{
-                            width: '100%',
-                            height: 400
-                        }} />
-                    </View>
-
-                    <View style={{
-                        borderWidth: 1,
-                        borderColor: 'green',
-                        height: 70
-                    }}>
-
-                    </View>
-                </View>
-            </View>
-        )
-    }
-    else if (ima == '') {
-        return (<View>
+    let ID = props.user
+    return (
+        <View>
             <View style={{  // status
                 // borderWidth: 1,
                 // borderColor: 'black',
                 height: 'auto',
                 marginVertical: 6,
                 justifyContent: 'center',
-                backgroundColor: 'white',
+                backgroundColor: 'white'
                 // alignItems: 'center',
             }}>
                 <View style={{ // avatar and content status
@@ -207,16 +70,10 @@ function Homeid(props) {
                     </Text>
                 </View>
 
+                {/* constants status return */}
+                <Constants_Home cons={ID} />
 
-                <View style={styles.container}>
-                    <Video source={{ uri: video }}
-                        style={styles.backgroundVideo}
-                        controls={true}
-                    />
-                </View>
-
-
-                <View style={{
+                <View style={{ // cmt and like
                     borderWidth: 1,
                     borderColor: 'green',
                     height: 70
@@ -225,9 +82,8 @@ function Homeid(props) {
                 </View>
             </View>
         </View>
-        )
+    )
 
-    }
 };
 var styles = StyleSheet.create({ // style video anh Minh cầu nguyện
     container: {
