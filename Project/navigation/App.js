7edcaff435ffbccd5fd5613a9fe9da884d6c8511
TaiKonne <taichuotchuoi@gmail.com>
2023-02-24 22:10:59 +0700

    update 24/02/23

---
+++ Project/navigation/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+import {
+    Image,
+    Text,
+    View,
+    ImageBackground,
+    TouchableOpacity,
+    ScrollView,
+    FlatList
+} from 'react-native'
+import { icons, images, colors } from '../constants/index.js'
+import { UIButton } from '../components'
+import Icon from 'react-native-vector-icons/dist/FontAwesome'
+import { Avatar, TextInput } from 'react-native-paper';
+
+function App(pros){
+    
+}
